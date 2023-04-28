56befdf6e250fba313799597b9e5467777f31223
Amin Matini <Aminmatini78@gmail.com>
2023-04-28 17:26:04 +0330

    feat(Usings.cs): add global usings for MediatR, FluentValidation, Microsoft.Extensions.Logging, TaskManagement.Domain.Repositories, and TaskManagement.Domain.UnitOfWorks.

---
+++ src/TaskManagement.Application/Usings.cs
@@ -0,0 +1,5 @@
+﻿global using MediatR;
+global using FluentValidation;
+global using Microsoft.Extensions.Logging;
+global using TaskManagement.Domain.Repositories;
+global using TaskManagement.Domain.UnitOfWorks;
