db47db55a6c5a5ac95c59a84c536b508f31cd8f0
Amin Matini <Aminmatini78@gmail.com>
2023-04-28 17:26:20 +0330

    feat(Usings.cs): add using statements for TaskManagement.Domain.Entities and TaskManagement.Domain.Repositories.Common

--- src/TaskManagement.Domain/Usings.cs
+++ src/TaskManagement.Domain/Usings.cs
@@ -3,3 +3,5 @@ global using System.Linq.Expressions;
 global using Microsoft.Data.SqlClient;
 global using TaskManagement.Common.LifeTimes;
 global using TaskManagement.Domain.Entities.Common;
+global using TaskManagement.Domain.Entities;
+global using TaskManagement.Domain.Repositories.Common;
