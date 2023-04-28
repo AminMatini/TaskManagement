37131a2966ebd8fa8c0fc97d563a8fc113a14606
Amin Matini <Aminmatini78@gmail.com>
2023-04-28 13:07:47 +0330

    feat(BaseEntity.cs): add CreateAt property to BaseEntity class

--- src/TaskManagement.Domain/Entities/Common/BaseEntity.cs
+++ src/TaskManagement.Domain/Entities/Common/BaseEntity.cs
@@ -3,6 +3,7 @@
 public abstract class BaseEntity<TKey> where TKey : IComparable
 {
     public TKey Id { get; set; } = default!;
+    public DateTime CreateAt { get; set; }
 }
 
 public abstract class BaseEntity : BaseEntity<Guid>
