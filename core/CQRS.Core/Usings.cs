725ab7fa844962a1e85a0b60a9169e6f9050627a
Amin Matini <Aminmatini78@gmail.com>
2023-05-12 23:00:28 +0330

    feat : add global usings

---
+++ core/CQRS.Core/Usings.cs
@@ -0,0 +1,8 @@
+﻿global using MongoDB.Bson;
+global using MongoDB.Bson.Serialization.Attributes;
+
+global using CQRS.Core.Messages;
+global using CQRS.Core.Events;
+global using CQRS.Core.Domain;
+global using CQRS.Core.Commands;
+global using CQRS.Core.Queries;
