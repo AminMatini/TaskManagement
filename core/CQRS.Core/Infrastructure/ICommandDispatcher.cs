a2823f861eee299c244e36d9f902166bf2f10e9a
Amin Matini <Aminmatini78@gmail.com>
2023-05-12 22:59:42 +0330

    feat : add event Infrastructure contracts

---
+++ core/CQRS.Core/Infrastructure/ICommandDispatcher.cs
@@ -0,0 +1,7 @@
+﻿namespace CQRS.Core.Infrastructure;
+
+public interface ICommandDispatcher
+{
+    void RegisterHandler<T>(Func<T, Task> handler) where T : BaseCommand;
+    Task SendAsync(BaseCommand command);
+}
