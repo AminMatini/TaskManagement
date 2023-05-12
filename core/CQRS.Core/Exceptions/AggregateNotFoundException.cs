61c2282f687f368f2bf1c02094426e8a82fec17f
Amin Matini <Aminmatini78@gmail.com>
2023-05-12 22:55:01 +0330

    feat : add exceptions model

---
+++ core/CQRS.Core/Exceptions/AggregateNotFoundException.cs
@@ -0,0 +1,9 @@
+﻿namespace CQRS.Core.Exceptions;
+
+public class AggregateNotFoundException : Exception
+{
+    public AggregateNotFoundException(string errMsg) : base(errMsg)
+    {
+            
+    }
+}
